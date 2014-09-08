8a39fb656be27145b94062d6df395291ba46b19b
ehsmohammadi <ehs.mohammadi@gmail.com>
2014-09-08 17:42:44 +0430

    Migration Added

---
+++ HomeWork.Persistance.Migrations/MigrationVer1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentMigrator;
+
+namespace HomeWork.Persistance.Migrations
+{
+    [Migration(1)]
+    public class MigrationVer1 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("TestEntities")
+                 .WithColumn("Id").AsInt64().PrimaryKey()
+                 .WithColumn("RowVersion").AsCustom("rowversion")
+                 .WithColumn("TestProperty").AsString(100).NotNullable();
+
+            Create.Table("TestEntityItems")
+                     .WithColumn("Id").AsInt64().PrimaryKey()
+                     .WithColumn("RowVersion").AsCustom("rowversion")
+                     .WithColumn("TestProperty").AsString(100).NotNullable()
+                     .WithColumn("TestEntityId").AsInt64().Nullable().ForeignKey("TestEntities","Id");
+
+            Create.Table("Entities")
+                 .WithColumn("Id").AsInt64().PrimaryKey()
+                 .WithColumn("RowVersion").AsCustom("rowversion")
+                 .WithColumn("TestProperty").AsString(100).NotNullable();
+
+            Create.Table("EntityItems")
+                     .WithColumn("Id").AsInt64().PrimaryKey()
+                     .WithColumn("RowVersion").AsCustom("rowversion")
+                     .WithColumn("TestProperty").AsString(100).NotNullable()
+                     .WithColumn("EntityId").AsInt64().Nullable().ForeignKey("Entities", "Id");
+
+            Create.Table("EntityItems2")
+                     .WithColumn("Id").AsInt64().PrimaryKey()
+                     .WithColumn("RowVersion").AsCustom("rowversion")
+                     .WithColumn("TestProperty").AsString(100).NotNullable()
+                     .WithColumn("EntityId").AsInt64().Nullable().ForeignKey("Entities", "Id");
+
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
